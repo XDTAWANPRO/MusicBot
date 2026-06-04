2cf47eda0e1c99cac0780e4d5d34469e8036cafe
XDTAWANPRO <77896938+XDTAWANPRO@users.noreply.github.com>
2026-06-04 15:03:19 +0700

    Update player.py

--- utils/player.py
+++ utils/player.py
@@ -21,12 +21,17 @@ FFMPEG_OPTS = {
 }
 
 _YDL_COMMON = {
-    "quiet": True, "no_warnings": True, "source_address": "0.0.0.0","cookiefile": "cookies.txt",
+    "quiet":          True,
+    "no_warnings":    True,
+    "source_address": "0.0.0.0",
+    "cookiefile":     "cookies.txt",
 }
-YDL_SEARCH = {**_YDL_COMMON, "format": "bestaudio/best", "noplaylist": True, "format_sort": ["abr", "asr"]}
-YDL_PLAYLIST = {**_YDL_COMMON, "format": "bestaudio/best", "noplaylist": False,
+YDL_SEARCH   = {**_YDL_COMMON, "noplaylist": True}
+YDL_PLAYLIST = {**_YDL_COMMON, "noplaylist": False,
                 "playlistend": 50, "extract_flat": "in_playlist"}
-YDL_STREAM = {**_YDL_COMMON, "format": "bestaudio/best", "noplaylist": True}
+YDL_STREAM   = {**_YDL_COMMON, "noplaylist": True,
+                "format": "bestaudio/best",
+                "format_sort": ["acodec:opus", "acodec:vorbis", "acodec:mp4a", "ext"]}
 
 
 # ── Song ──────────────────────────────────────────────────────────────────────
